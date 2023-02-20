5419a7e16c6cec048d2b58f7f294523940b6747e
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-02-20 11:47:14 +0200

    clone repository

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      а это вторая часть магии !!!!Phonebook!!!!фывфывфывфывфывфыв
     </div>
   );
 };
